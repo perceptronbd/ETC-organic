e308e3753804d23e1ce11d461d506fcd65847955
Asif Vudi <asifaslam.aa81@gmail.com>
2023-08-03 02:20:59 +0600

    Update Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Home, Login, Overview, ProductList } from "./pages";
 
 export default function App() {
   return (
     <Routes>
+      <Route exact path="/login" element={<Login />} />
       <Route exact path="/" element={<Home />}>
-        <Route exact path="overview" element={<Overview />} />
-        <Route exact path="product-list" element={<ProductList />} />
+        <Route path="overview" element={<Overview />} />
+        <Route path="product-list" element={<ProductList />} />
       </Route>
     </Routes>
   );
