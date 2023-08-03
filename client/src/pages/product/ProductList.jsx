31cd480c52948b8409c28cad96c8122caa7f18d0
Asif Vudi <asifaslam.aa81@gmail.com>
2023-08-04 04:37:36 +0600

    Update ProductList.jsx: Added button

--- client/src/pages/product/ProductList.jsx
+++ client/src/pages/product/ProductList.jsx
@@ -1,5 +1,22 @@
 import React from "react";
+import { Button, Text } from "../../components";
 
 export const ProductList = () => {
-  return <div>ProductList</div>;
+  return (
+    <div className="w-full m-4 3xl:mx-28">
+      <div className="flex justify-between">
+        <Text h1>Product List</Text>
+        <Button className={`h-8`}>Add Product</Button>
+      </div>
+      <div className="bg-red-500 w-full flex">
+        <div className="h-8 bg-green-100 opacity-40 w-[4%]"></div>
+        <div className="h-8 bg-green-200 opacity-40 w-[14%]"></div>
+        <div className="h-8 bg-green-300 opacity-40 w-[16%]"></div>
+        <div className="h-8 bg-green-400 opacity-40 w-[10%]"></div>
+        <div className="h-8 bg-green-500 opacity-40 w-[7%]"></div>
+        <div className="h-8 bg-green-600 opacity-40 w-[43%]"></div>
+        <div className="h-8 bg-green-700 opacity-40 w-[6%]"></div>
+      </div>
+    </div>
+  );
 };
