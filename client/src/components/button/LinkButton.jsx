a76adc7f09baf96eee3bd1448f72197029fca65f
Asif Vudi <asifaslam.aa81@gmail.com>
2023-08-05 03:37:52 +0600

    Add Icon

--- client/src/components/button/LinkButton.jsx
+++ client/src/components/button/LinkButton.jsx
@@ -3,17 +3,20 @@ import { cw } from "../../utils/cw";
 import { Link } from "react-router-dom";
 
 export const LinkButton = (props) => {
-  const { children, className } = props;
+  const { children, className, icon: Icon } = props;
   return (
     <Link to={props.to}>
       <button
         {...props}
         className={cw(
-          `bg-accent-primary w-52 h-12 rounded-lg font-semibold text-white hover:bg-opacity-80 transition-all ease-in-out duration-300`,
+          `bg-accent-primary w-52 h-12 px-4 rounded-lg font-semibold text-white hover:bg-opacity-80 transition-all ease-in-out duration-300 `,
           className
         )}
       >
-        {children}
+        <span className="flex justify-center items-center">
+          {Icon && <Icon className="w-5 h-5" />}
+          {children}
+        </span>
       </button>
     </Link>
   );
