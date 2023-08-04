43a354cfef0e3e2942b46557c8fd403c07951758
Asif Vudi <asifaslam.aa81@gmail.com>
2023-08-05 04:33:26 +0600

    Update UI

--- client/src/components/input/FormInput.jsx
+++ client/src/components/input/FormInput.jsx
@@ -6,13 +6,17 @@ export const FormInput = (props) => {
 
   return (
     <div className="flex flex-col my-2">
-      {label && <label htmlFor={id}>{label}</label>}
+      {label && (
+        <label htmlFor={id} className="font-semibold mb-2">
+          {label}
+        </label>
+      )}
       <input
         id={id}
         {...inputProps}
         onChange={onChange}
         className={cw(
-          "peer border rounded-full w-72 p-2 focus:outline-none focus:ring-1 focus:border-accent-secondary placeholder:px-2",
+          "peer border rounded-xl w-72 p-2 focus:outline-none focus:ring-1 focus:border-accent-secondary placeholder:px-2",
           className
         )}
       />
