0d4c643d203baac987159d63fa4b6c03c9c2bb3e
Asif Vudi <asifaslam.aa81@gmail.com>
2023-08-06 20:15:27 +0600

    Update: Added scrollview and padding

--- client/src/components/container/Container.jsx
+++ client/src/components/container/Container.jsx
@@ -4,6 +4,13 @@ import { cw } from "../../utils/cw";
 export const Container = (props) => {
   const { children, className } = props;
   return (
-    <div className={cw("w-full m-4 3xl:mx-28", className)}>{children}</div>
+    <div
+      className={cw(
+        "w-full m-4 3xl:mx-28 h-[97vh] p-2 overflow-x-auto",
+        className
+      )}
+    >
+      {children}
+    </div>
   );
 };
