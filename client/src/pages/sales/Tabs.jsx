ce61425a5b33054b58297491ad7655ae2165daf6
Asif Vudi <asifaslam.aa81@gmail.com>
2023-08-08 21:38:28 +0600

    Fix hover effect

--- client/src/pages/sales/Tabs.jsx
+++ client/src/pages/sales/Tabs.jsx
@@ -4,7 +4,7 @@ const Tab = ({ label, isActive, onClick }) => {
   return (
     <button
       className={`${
-        isActive ? "bg-white" : " "
+        isActive ? "bg-white hover:bg-white" : " "
       } text-accent-primary font-semibold px-4 py-2 rounded-t-md focus:outline-none hover:bg-gray-200 transition-all ease-in-out duration-300`}
       onClick={onClick}
     >
