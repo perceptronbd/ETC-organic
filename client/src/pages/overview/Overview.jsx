797fc5c1bd87a56c9d1c4991270f470929aa6ec0
Asif Vudi <asifaslam.aa81@gmail.com>
2023-08-19 01:02:17 +0600

    Update Overview.jsx

--- client/src/pages/overview/Overview.jsx
+++ client/src/pages/overview/Overview.jsx
@@ -1,6 +1,35 @@
 import React from "react";
-import { Container } from "../../components";
+import { Container, Text } from "../../components";
+import { BiLogOut } from "react-icons/bi";
+import { useAuth } from "../../context/AuthContext";
 
 export const Overview = () => {
-  return <Container>Overview</Container>;
+  const { logout } = useAuth();
+
+  return (
+    <Container>
+      <div className="flex justify-between items-center rounded-lg">
+        <section className="rounded-lg w-fit">
+          <Text h2>
+            <span className="text-textColor-light">Welcome to</span>{" "}
+            <span className="text-accent-secondary"> ETC Organic</span>{" "}
+            <span className="text-accent-primary">ERP!</span>
+          </Text>
+        </section>
+        <section className="flex items-center w-fit px-4 py-2 bg-foreground rounded-lg">
+          <Text h3>User name</Text>
+          <button
+            onClick={() => {
+              logout();
+            }}
+          >
+            <BiLogOut
+              className="ml-4 hover:cursor-pointer bg-background hover:bg-accent-secondary hover:text-foreground transition-all ease-in-out duration-300 rounded-md p-1"
+              size={"34px"}
+            />
+          </button>
+        </section>
+      </div>
+    </Container>
+  );
 };
