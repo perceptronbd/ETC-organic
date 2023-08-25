b8797a5384eba8f34669a6c11cadc619ea0112e0
Asif Vudi <asifaslam.aa81@gmail.com>
2023-08-25 21:16:20 +0600

    Make height responsive

--- client/src/components/card/SalesCard.jsx
+++ client/src/components/card/SalesCard.jsx
@@ -32,13 +32,13 @@ export const SalesCard = (props) => {
   return (
     <div
       className={cw(
-        "grid grid-rows-5 grid-cols-2 gap-2 bg-foreground rounded-lg px-3 py-2 h-20 3xl:h-28",
+        "grid grid-rows-5 grid-cols-2 gap-2 bg-foreground rounded-lg px-3 py-2 h-20 3xl:h-40",
         className
       )}
     >
       <Icon
         className={cw(
-          "w-6 h-6 3xl:w-8 3xl:h-8 p-1 rounded-lg text-foreground 3xl:row-start-1 ",
+          "w-6 h-6 3xl:w-10 3xl:h-10 p-1 rounded-lg text-foreground 3xl:row-start-1 ",
           {
             "bg-yellow-500": type === "Total Sales",
             "bg-green-500": type === "Online Sales",
@@ -46,13 +46,15 @@ export const SalesCard = (props) => {
           }
         )}
       />
-      <Text className={"3xl:row-end-6  text-textColor-light text-sm "}>
+      <Text
+        className={"3xl:row-end-6 text-textColor-light text-sm 3xl:text-lg"}
+      >
         {type}
       </Text>
 
       <Text
         h2
-        className={"3xl:font-bold 3xl:text-3xl row-end-4 3xl:row-start-3"}
+        className={"3xl:font-bold 3xl:text-5xl row-end-4 3xl:row-start-3"}
       >
         {formattedAmount}
       </Text>
