779b2f802ee5e22ee8321c6e062dff68c71922d8
acesuu <ahnafakib@iut-dhaka.edu>
2023-11-27 02:05:03 +0600

    completed checkout and order feature

--- server/Mobile_app/routes/userRoutes.js
+++ server/Mobile_app/routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/mobileUserController');
 const profileUpdateController = require('../controllers/profileUpdateController');
 const cartController = require('../controllers/cartController');
+const orderController = require('../controllers/orderController');
 const upload = require('../middleware/imageMiddleware');
 const authenticateUser = require('../middleware/authMiddleware'); 
 
@@ -19,6 +20,10 @@ router.post('/decrease-quantity',authenticateUser,cartController.decreaseQuantit
 router.post('/update-cart',authenticateUser,cartController.updateCart);
 router.get('/get-cart-price',authenticateUser,cartController.getTotalPrice);
 router.get('/get-cart-details',authenticateUser,cartController.getCartDetails);
+
+//order
+router.post('/place-order',authenticateUser,orderController.placeOrder);
+router.get('/get-user-order-details',authenticateUser,orderController.getOrderDetails);
 
 
 module.exports = router; 
