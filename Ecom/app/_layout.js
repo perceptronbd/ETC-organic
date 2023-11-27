9d6627c77de37d4d93e27d495d2831fc225c87ac
Asif Vudi <asifaslam.aa81@gmail.com>
2023-11-28 01:59:07 +0600

    create layout

---
+++ Ecom/app/_layout.js
@@ -0,0 +1,22 @@
+import { Stack } from "expo-router";
+
+export default function Layout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#f4511e",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    >
+      <Stack.Screen
+        name="home"
+        options={{ title: "Home", headerShown: false }}
+      />
+    </Stack>
+  );
+}
