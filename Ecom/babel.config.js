66ed0c8e81a03f16012b01e928c1f6cc733af31f
Asif Vudi <asifaslam.aa81@gmail.com>
2023-11-28 01:58:48 +0600

    Install initial dependencies

---
+++ Ecom/babel.config.js
@@ -0,0 +1,13 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: ["expo-router/babel"],
+    plugins: ["nativewind/babel"],
+    env: {
+      production: {
+        plugins: ["react-native-paper/babel"],
+      },
+    },
+  };
+};
