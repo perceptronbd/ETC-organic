da86b0a0d777a4065eb58bab300f707fcf43c768
Asif Vudi <asifaslam.aa81@gmail.com>
2023-11-29 00:44:34 +0600

    Add Montserrat fonts

--- Ecom/app/_layout.js
+++ Ecom/app/_layout.js
@@ -1,16 +1,34 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function Layout() {
+  const [loaded, error] = useFonts({
+    mon: require("../assets/fonts/Montserrat-Regular.ttf"),
+    "mon-m": require("../assets/fonts/Montserrat-Medium.ttf"),
+    "mon-b": require("../assets/fonts/Montserrat-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <Stack
       screenOptions={{
-        headerStyle: {
-          backgroundColor: "#f4511e",
-        },
-        headerTintColor: "#fff",
-        headerTitleStyle: {
-          fontWeight: "bold",
-        },
+        headerShown: false,
       }}
     >
       <Stack.Screen
