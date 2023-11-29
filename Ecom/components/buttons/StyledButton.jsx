2667c752f1e3a09f7422e940db29ff4ab15d57d4
Asif Vudi <asifaslam.aa81@gmail.com>
2023-11-30 02:15:14 +0600

    Refactor StyledButton component

--- Ecom/components/buttons/StyledButton.jsx
+++ Ecom/components/buttons/StyledButton.jsx
@@ -1,22 +1,37 @@
 import { StyledComponent } from "nativewind";
 import React from "react";
-import { Button } from "react-native-paper";
+import { Text } from "react-native-paper";
 import COLOR from "../../constants/COLOR";
+import { Pressable, View } from "react-native";
 
 export const StyledButton = ({ children, ...props }) => {
   const { variant, className, ...rest } = props;
 
   return (
-    <StyledComponent
-      component={Button}
-      {...rest}
-      tw={`w-52 h-12 justify-center items-center rounded-lg ${className}`}
-      mode={variant === "outlined" ? "outlined" : "contained"}
-      textColor={variant === "outlined" ? COLOR.secondary : COLOR.foreground}
-      buttonColor={variant === "outlined" ? "" : COLOR.secondary}
-      rippleColor={variant === "outlined" ? COLOR.secondary : COLOR.foreground}
-    >
-      {children}
-    </StyledComponent>
+    <View style={{ borderRadius: 10, overflow: "hidden" }}>
+      <StyledComponent
+        component={Pressable}
+        tw={`w-64 h-12 justify-center items-center ${className}`}
+        style={{
+          backgroundColor: variant === "secondary" ? "" : COLOR.secondary,
+          borderColor: COLOR.secondary,
+          borderRadius: 10,
+          borderWidth: 1,
+        }}
+        android_ripple={{
+          color: COLOR.tertiary,
+        }}
+        {...rest}
+      >
+        <StyledComponent
+          component={Text}
+          style={{
+            color: variant === "secondary" ? COLOR.secondary : COLOR.foreground,
+          }}
+        >
+          Button
+        </StyledComponent>
+      </StyledComponent>
+    </View>
   );
 };
