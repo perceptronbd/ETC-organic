93f5bebb2b6638afb89fe1d91c52c5e6c66f7a09
Asif Vudi <asifaslam.aa81@gmail.com>
2023-12-02 02:41:47 +0600

    Refactor StyledButton component to use Button from react-native-paper

--- Ecom/components/buttons/StyledButton.jsx
+++ Ecom/components/buttons/StyledButton.jsx
@@ -1,39 +1,30 @@
 import React from "react";
-import { Text } from "react-native-paper";
+import { Button } from "react-native-paper";
 import COLOR from "../../constants/COLOR";
-import { Pressable, View } from "react-native";
 
 export const StyledButton = ({ children, ...props }) => {
-  const { variant, style, ...rest } = props;
+  const { variant, style, contentStyle, ...rest } = props;
 
   return (
-    <View style={{ borderRadius: 10, overflow: "hidden" }}>
-      <Pressable
-        style={{
-          width: 200,
-          height: 50,
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: variant === "secondary" ? "" : COLOR.secondary,
-          borderColor: COLOR.secondary,
-          borderRadius: 10,
-          borderWidth: 1,
-          margin: 10,
-          ...style,
-        }}
-        android_ripple={{
-          color: COLOR.tertiary,
-        }}
-        {...rest}
-      >
-        <Text
-          style={{
-            color: variant === "secondary" ? COLOR.secondary : COLOR.foreground,
-          }}
-        >
-          {children}
-        </Text>
-      </Pressable>
-    </View>
+    <Button
+      buttonColor={COLOR.secondary}
+      style={{
+        margin: 8,
+        borderRadius: 10,
+        ...style,
+      }}
+      contentStyle={{
+        width: 250,
+        height: 50,
+        justifyContent: "center",
+        alignItems: "center",
+        ...contentStyle,
+      }}
+      textColor={"white"}
+      rippleColor={COLOR.tertiary}
+      onPress={() => console.log("Pressed")}
+    >
+      {children}
+    </Button>
   );
 };
