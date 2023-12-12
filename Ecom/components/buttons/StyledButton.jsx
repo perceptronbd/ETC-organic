e57b5522ff38fb53eda941e31aa7632d36653bfb
Asif Vudi <asifaslam.aa81@gmail.com>
2023-12-13 00:41:33 +0600

    Fix button styling and toast width

--- Ecom/components/buttons/StyledButton.jsx
+++ Ecom/components/buttons/StyledButton.jsx
@@ -10,13 +10,13 @@ export const StyledButton = ({ children, ...props }) => {
     <Button
       buttonColor={variant === "outline" ? null : COLOR.secondary}
       style={tw.style(
-        "mt-2  h-10 items-center justify-center rounded-xl",
+        "mt-2 h-10 items-center justify-center rounded-xl",
         style,
         {
           "border border-black": variant === "outline",
         },
       )}
-      contentStyle={tw.style(`h-10 w-64 `, contentStyle)}
+      contentStyle={tw.style(`w-96`, contentStyle)}
       textColor={variant === "outline" ? "black" : "white"}
       maxFontSizeMultiplier={1}
       rippleColor={COLOR.tertiary}
--- Ecom/components/toast/StyledToast.jsx
+++ Ecom/components/toast/StyledToast.jsx
@@ -1,14 +1,21 @@
 import { MaterialIcons } from "@expo/vector-icons";
 import React from "react";
-import { View } from "react-native";
+import { Dimensions, View } from "react-native";
 import tailwind from "twrnc";
 import { StyledText } from "../texts/StyledText";
 
 export const StyledToast = ({ title, description, variant }) => {
+  const { width } = Dimensions.get("window");
+
+  console.log(width);
+
   return (
     <View
       style={tailwind.style(
-        `w-96 flex-row gap-4 items-center bg-white shadow-md rounded-md py-2 px-4`,
+        `flex-row gap-4 items-center bg-white shadow-md rounded-md py-2 px-4`,
+        {
+          width: width - 32,
+        },
         {
           "border-l-2 border-r-2 border-green-500": variant === "success",
           "border-l-2 border-r-2 border-yellow-500": variant === "warning",
