063670ffffef77074f551527490285f89e629355
Asif Vudi <asifaslam.aa81@gmail.com>
2023-12-12 20:44:17 +0600

    Add product details to mockData.js

--- Ecom/constants/mockData.js
+++ Ecom/constants/mockData.js
@@ -2,6 +2,8 @@ export const favoriteProduct = [
   {
     id: 0,
     name: "তুলশি বীজ জুস",
+    details:
+      "DetaLorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac sapien nec metus imperdiet hendrerit vel eget mauris. Sed metus augue, sollicitudin ac nulla sit amet, tincidunt laoreet eros.",
     img: require("../assets/product/tulshi.png"),
     price: "৫০০",
     csb: "৮০",
@@ -11,6 +13,8 @@ export const favoriteProduct = [
   {
     id: 2,
     name: "কারকুমা জয়েন্ট গার্ড",
+    details:
+      "DetaLorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac sapien nec metus imperdiet hendrerit vel eget mauris. Sed metus augue, sollicitudin ac nulla sit amet, tincidunt laoreet eros.",
     img: require("../assets/product/karkuma.png"),
     price: "৫৮০",
     csb: "৮০",
@@ -20,6 +24,8 @@ export const favoriteProduct = [
   {
     id: 3,
     name: "Lotion",
+    details:
+      "DetaLorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac sapien nec metus imperdiet hendrerit vel eget mauris. Sed metus augue, sollicitudin ac nulla sit amet, tincidunt laoreet eros.",
     img: require("../assets/product/lotion.png"),
     price: "৫০০",
     csb: "৮০",
@@ -29,6 +35,8 @@ export const favoriteProduct = [
   {
     id: 4,
     name: "কারকুমা জয়েন্ট গার্ড",
+    details:
+      "DetaLorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac sapien nec metus imperdiet hendrerit vel eget mauris. Sed metus augue, sollicitudin ac nulla sit amet, tincidunt laoreet eros.",
     img: require("../assets/product/karkuma.png"),
     price: "৫৮০",
     csb: "৮০",
@@ -38,6 +46,8 @@ export const favoriteProduct = [
   {
     id: 5,
     name: "Lotion",
+    details:
+      "DetaLorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac sapien nec metus imperdiet hendrerit vel eget mauris. Sed metus augue, sollicitudin ac nulla sit amet, tincidunt laoreet eros.",
     img: require("../assets/product/lotion.png"),
     price: "৫০০",
     csb: "৮০",
