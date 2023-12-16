10316ec12d9b60a1984a2b4f0f4f139aedb27cfc
Asif Vudi <asifaslam.aa81@gmail.com>
2023-12-16 20:06:00 +0600

    Add GoBack button component

--- Ecom/components/index.js
+++ Ecom/components/index.js
@@ -2,6 +2,7 @@
 export { StyledInput } from "./inputs/StyledInput";
 //buttons
 export { Counter } from "./buttons/Counter";
+export { GoBack } from "./buttons/GoBack";
 export { StyledButton } from "./buttons/StyledButton";
 //texts
 export { StyledText } from "./texts/StyledText";
