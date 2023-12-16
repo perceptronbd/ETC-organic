31c2259529b3ff0dd311bc09b0a3cd167ef2d4c0
Asif Vudi <asifaslam.aa81@gmail.com>
2023-12-17 05:11:51 +0600

    Refactor referEarn.js: Update imports and add navigation functionality

--- Ecom/app/(drawer)/referEarn.js
+++ Ecom/app/(drawer)/referEarn.js
@@ -1,9 +1,20 @@
-import { View, Text } from "react-native";
+import { router } from "expo-router";
 import React from "react";
+import { Pressable, Text, View } from "react-native";
+import tailwind from "twrnc";
+import { StyledText } from "../../components";
 
 export default function referEarn() {
   return (
-    <View>
+    <View style={tailwind`flex-1 p-4`}>
+      <Pressable
+        onPress={() => {
+          console.log("referEarn route");
+          router.replace("/(tabs)/home");
+        }}
+      >
+        <StyledText>Refer and Earn</StyledText>
+      </Pressable>
       <Text>referEarn</Text>
     </View>
   );
