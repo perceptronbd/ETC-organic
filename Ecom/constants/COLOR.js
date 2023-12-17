fb19d20348509861e769d47d5d9371bc370efb58
Asif Vudi <asifaslam.aa81@gmail.com>
2023-12-18 02:10:09 +0600

    Add new light colors to COLOR.js

--- Ecom/constants/COLOR.js
+++ Ecom/constants/COLOR.js
@@ -1,6 +1,8 @@
 export default {
   primary: "#A259FF",
+  primaryLight: "#E8D6FF",
   secondary: "#0C904D",
+  secondaryLight: "#E8FFF0",
   tertiary: "#239F95",
   background: "#f8f8f8",
   foreground: "#ffffff",
