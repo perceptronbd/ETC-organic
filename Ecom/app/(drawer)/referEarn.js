2af945d368440cb83a1d0c00d9a4236fc17c5218
Asif Vudi <asifaslam.aa81@gmail.com>
2023-12-19 23:10:02 +0600

    Add Share functionality to referEarn screen

--- Ecom/app/(drawer)/referEarn.js
+++ Ecom/app/(drawer)/referEarn.js
@@ -1,6 +1,6 @@
 import { router } from "expo-router";
 import React from "react";
-import { View } from "react-native";
+import { Alert, Share, View } from "react-native";
 import { Divider } from "react-native-paper";
 import tailwind from "twrnc";
 import {
@@ -13,14 +13,37 @@ import COLOR from "../../constants/COLOR";
 import { useModal } from "../../hooks";
 
 export default function referEarn() {
+  const message = `হ্যালো,
+  ETC Organic অ্যাপের ‘Refer and Earn’ প্রোগ্রামের খাতিরে, আমি আপনাকে তাদের অ্যাপ ইনস্টল করার জন্য হাইলি রিকমেন্ড করছি।
+  
+  এটি একটি অসাধারণ অ্যাপ। অ্যাপটি ইন্সটল করার মাধ্যমে, আপনি বিভিন্ন অর্গানিক পণ্য ক্রয় করার সুযোগ তো পাবেন-ই, সেই সাথে আমার মতো তাদের ‘রেফার অ্যান্ড আর্ন প্রোগ্রামে’ যোগ দিয়ে আজীবন নিশ্চিত উপার্জনের সুযোগ-ও করে নিতে পারবেন।
+  তাই আর দেরি না করে অ্যাপটি আজই ইন্সটল করুন এবং সাইন আপ করুন। সাইন আপ করার সময় অবশ্যই আমার রেফার কোড 86572  টি ইনপুট করুন।
+  
+  অ্যাপ ইন্সটল লিংক: googleplaystore/etcorganicapp`;
+
   const {
     visible: isAlert,
     showModal: showAlert,
     hideModal: hideAlert,
   } = useModal();
 
-  const onRefer = () => {
-    showAlert();
+  const onRefer = async () => {
+    try {
+      const result = await Share.share({
+        message: message,
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          // shared with activity type of result.activityType
+        } else {
+          // shared
+        }
+      } else if (result.action === Share.dismissedAction) {
+        // dismissed
+      }
+    } catch (error) {
+      Alert.alert(error.message);
+    }
   };
 
   const gotoProfile = () => {
