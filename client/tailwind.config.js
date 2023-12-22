cb5ff38aefcb956a8dd48b9bd02c20de91e9b721
Asif Vudi <asifaslam.aa81@gmail.com>
2023-12-22 21:52:42 +0600

    Update styles and configuration files

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,25 +1,70 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
+  ],
   theme: {
-    extend: {
+    container: {
+      center: true,
+      padding: "2rem",
       screens: {
-        "3xl": { min: "1367px" },
+        "2xl": "1400px",
       },
+    },
+    extend: {
       colors: {
-        background: "#F8F8F8",
-        foreground: "#FFFFFF",
-        textColor: { DEFAULT: "#303030", light: "#9a9a9a" },
+        background: "hsl(0, 0%, 95%)",
+        foreground: "hsl(0, 0%, 100%)",
+        textColor: { DEFAULT: "hsl(0, 0%, 20%)", light: "hsl(0, 0%, 16%)" },
+        primary: {
+          DEFAULT: "hsl(150, 85%, 31%)",
+          light: "hsl(150, 50%, 60%)",
+        },
+        secondary: {
+          DEFAULT: "hsl(175, 64%, 38%)",
+          light: "hsl(175, 44%, 50%)",
+        },
         accent: {
-          primary: "#A259FF",
-          secondary: "#0C904D",
+          DEFAULT: "hsl(266, 100%, 67%)",
+          secondary: "hsl(266, 100%, 80%)",
+        },
+        destructive: {
+          DEFAULT: "hsl(0 62.8% 30.6%)",
+          foreground: "hsl(210 40% 98%)",
+        },
+        muted: {
+          DEFAULT: "hsl(210 40% 96.1%)",
+          foreground: "hsl(222.2 47.4% 11.2%)",
         },
       },
       fontFamily: {
         sans: ["Poppins", "sans-serif"],
         montserrat: ["Montserrat", "sans-serif"],
       },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
