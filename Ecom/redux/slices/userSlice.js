898bde7127bcb49edda8b25711fd5607cf970d42
Asif Vudi <asifaslam.aa81@gmail.com>
2023-12-27 05:25:11 +0600

    Add @reduxjs/toolkit and react-redux dependencies and create userSlice

---
+++ Ecom/redux/slices/userSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    user: null,
+    isSignedIn: false,
+    loading: false,
+    error: false,
+    message: "",
+  },
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
+    setIsSignedIn: (state, action) => {
+      state.isSignedIn = action.payload;
+    },
+  },
+});
+
+export const { setUser, setIsSignedIn } = userSlice.actions;
+
+export const selectUser = (state) => state.user;
+
+export const selectIsSignedIn = (state) => state.user.isSignedIn;
+
+export default userSlice.reducer;
