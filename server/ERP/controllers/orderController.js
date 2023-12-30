22ab7db6130ad9d9f7b30be0af03d13f0e5ff49c
kowshikjawad <jawadkowshik@gmail.com>
2023-12-30 07:44:53 +0600

    Modify display orders according to requirements

--- server/ERP/controllers/orderController.js
+++ server/ERP/controllers/orderController.js
@@ -1,5 +1,9 @@
-
 const Order = require('../../models/orderModel'); // Import the Order model
+const mobileUser = require('../../models/mobileUserModel'); // Import the mobileUser model
+const Cart = require('../../models/cartModel'); // Import the cart model
+const Checkout = require('../../models/checkoutModel'); // Import the checkout model
+//import asynchandler
+const asyncHandler = require('express-async-handler');
 
 exports.getAllOrders = async (req, res) => {
   try {
@@ -11,3 +15,28 @@ exports.getAllOrders = async (req, res) => {
     res.status(500).json({ message: 'Error fetching orders' }); // Send error response
   }
 };
+
+
+exports.displayOrders = asyncHandler(async (req, res) => {
+  try {
+    const orders = await Order.find()
+      .populate({
+        path: 'cart',
+        model: Cart, // replace with your Cart model name if different
+        populate: {
+          path: 'user',
+          model: mobileUser // replace with your User model name if different
+        }
+      })
+      .populate({
+        path: 'checkoutDetails',
+        model: Checkout // replace with your Checkout model name if different
+      });
+
+    console.log(orders);
+    res.status(200).json(orders);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error fetching orders' });
+  }
+});
--- server/ERP/routes/orderRouter.js
+++ server/ERP/routes/orderRouter.js
@@ -4,8 +4,9 @@ const router = express.Router();
 const { salesManagement } = require("../middleware/authMiddleware");
 const { checkLogin } = require("../middleware/checkLogin");
 
-const { getAllOrders } = require("../../ERP/controllers/orderController");
+const { getAllOrders, displayOrders} = require("../../ERP/controllers/orderController");
 
 router.get("/getMobileUserOrders", checkLogin, salesManagement, getAllOrders);
+router.get("/displayOrders", checkLogin, salesManagement, displayOrders);
 
 module.exports = router;
