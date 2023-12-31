acbca0d02921abf8f18548aef32f836fff819acc
kowshikjawad <jawadkowshik@gmail.com>
2023-12-31 07:56:27 +0600

    add getOnlineOrders

--- server/ERP/controllers/orderController.js
+++ server/ERP/controllers/orderController.js
@@ -2,6 +2,7 @@ const Order = require('../../models/orderModel'); // Import the Order model
 const mobileUser = require('../../models/mobileUserModel'); // Import the mobileUser model
 const Cart = require('../../models/cartModel'); // Import the cart model
 const Checkout = require('../../models/checkoutModel'); // Import the checkout model
+const Branch = require('../../models/branchModel'); // Import the branch model
 //import asynchandler
 const asyncHandler = require('express-async-handler');
 
@@ -38,5 +39,21 @@ exports.displayOrders = asyncHandler(async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Error fetching orders' });
+  }
+});
+
+exports.getOnlineOrders = asyncHandler(async(req, res) => {
+  try {
+    const branchId = "658f3e49b2e456d220b116a5"
+    const onlineBranch = await Branch.findOne({ _id: branchId })
+
+    if (!onlineBranch) {
+      res.status(404).json({ message: 'Branch not found' });
+    }
+
+    res.status(200).json(onlineBranch);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error fetching online branch' });
   }
 });
--- server/ERP/routes/orderRouter.js
+++ server/ERP/routes/orderRouter.js
@@ -4,9 +4,12 @@ const router = express.Router();
 const { salesManagement } = require("../middleware/authMiddleware");
 const { checkLogin } = require("../middleware/checkLogin");
 
-const { getAllOrders, displayOrders} = require("../../ERP/controllers/orderController");
+const { getAllOrders, displayOrders, getOnlineOrders} = require("../../ERP/controllers/orderController");
 
 router.get("/getMobileUserOrders", checkLogin, salesManagement, getAllOrders);
 router.get("/displayOrders", checkLogin, salesManagement, displayOrders);
+router.get("/getOnlineOrders", checkLogin, salesManagement, getOnlineOrders);
+
+
 
 module.exports = router;
