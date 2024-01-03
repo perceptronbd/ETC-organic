6d6b857292b068911598056b961b8d6422c8d2af
Asif Vudi <asifaslam.aa81@gmail.com>
2024-01-03 23:25:34 +0600

    Refactor fetchProducts function to improve logging

--- Ecom/api/cart/product.js
+++ Ecom/api/cart/product.js
@@ -1,13 +1,14 @@
 import { authURL } from "../instances/authURL";
 
 export const fetchProducts = async (token) => {
-  console.log("fetchProducts...");
   try {
     const res = await authURL(token).get("/get-products");
-    console.log("response:", res);
+    console.log("=======fetchProducts response=======");
+    console.log(res);
     return res;
   } catch (error) {
-    console.log("error:", error.response);
+    console.log("=======fetchProducts error=======");
+    console.log(error);
     const errorResponse = error.response;
     return errorResponse;
   }
