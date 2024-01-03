d56b3f7d9aa2d513165fb0da09cfda3f1ee929eb
kowshikjawad <jawadkowshik@gmail.com>
2024-01-03 19:47:48 +0600

    create commonController and walletHistory for ERP

--- server/CommonControllers/getProducts.js
+++ server/CommonControllers/getProducts.js
@@ -1,8 +1,10 @@
 const asyncHandler = require("express-async-handler");
 const Product = require("../models/productModel");
 
-
 exports.getAllProducts = asyncHandler(async(req,res) => {
     const allProducts = await Product.find({})
     res.status(200).json(allProducts)
   })
+
+
+  //Second Pr to server 
