f985f3a1ac95d53c45257b31a8041992af005974
Asif Vudi <asifaslam.aa81@gmail.com>
2024-01-04 23:53:58 +0600

    add updateCart and incQty, decQty

--- Ecom/contexts/CartContext.js
+++ Ecom/contexts/CartContext.js
@@ -1,5 +1,10 @@
 import React, { createContext, useEffect, useState } from "react";
-import { addToCart, getCartDetails } from "../api";
+import {
+  addToCart,
+  decreaseQuantity,
+  getCartDetails,
+  increaseQuantity,
+} from "../api";
 
 const CartContext = createContext();
 
@@ -75,28 +80,86 @@ export const CartProvider = ({ children }) => {
         error,
       );
     }
-    // Check if the product already exists in the cart
-    // const existingProductIndex = products.findIndex((p) => p.id === _id);
+  };
 
-    // if (existingProductIndex !== -1) {
-    //   // If the product exists, update its quantity
-    //   const updatedProducts = [...products];
-    //   updatedProducts[existingProductIndex].quantity = quantity;
-    //   setProducts(updatedProducts);
-    // } else {
-    //   // If the product is new, add it to the cart
-    //   setProducts([...products, details]);
-    // }
+  const incQty = async (productId) => {
+    try {
+      setLoading(true);
+      increaseQuantity(productId).then((res) => {
+        console.log("...CartContext increaseQuantity addToCart response:", res);
+        const { status } = res;
+        if (status === 200) {
+          getCartDetails().then((res) => {
+            console.log(
+              "...CartContext increaseQuantity fetchCartDetails response:",
+              res,
+            );
+            const { data, status } = res;
+            if (status === 200) {
+              setCart(data);
+              setProducts(data.products);
+            } else {
+              setLoading(false);
+              setMessage(data.message);
+            }
+          });
+          setLoading(false);
+          setMessage("পণ্য কার্ট যোগ করা হয়েছে");
+        } else {
+          setLoading(false);
+          setMessage("Something went wrong");
+        }
+        setLoading(false);
+      });
+    } catch (error) {
+      console.log("...CartContext increaseQuantity error:", error);
+    }
+  };
+
+  const decQty = async (productId) => {
+    try {
+      setLoading(true);
+      decreaseQuantity(productId).then((res) => {
+        console.log("...CartContext decreaseQuantity addToCart response:", res);
+        const { status } = res;
+        if (status === 200) {
+          getCartDetails().then((res) => {
+            console.log(
+              "...CartContext decreaseQuantity fetchCartDetails response:",
+              res,
+            );
+            const { data, status } = res;
+            if (status === 200) {
+              setCart(data);
+              setProducts(data.products);
+            } else {
+              setLoading(false);
+              setMessage(data.message);
+            }
+          });
+          setLoading(false);
+          setMessage("পণ্য কার্ট যোগ করা হয়েছে");
+        } else {
+          setLoading(false);
+          setMessage("Something went wrong");
+        }
+        setLoading(false);
+      });
+    } catch (error) {
+      console.log("...CartContext decreaseQuantity error:", error);
+    }
   };
 
   return (
     <CartContext.Provider
       value={{
         products,
-        updateProductDetails,
         cart,
         loading,
         message,
+        updateProductDetails,
+        incQty,
+        decQty,
       }}
     >
       {children}
