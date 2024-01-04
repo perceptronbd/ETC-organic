758827a123f54509dd4831dfecd7860a34961d67
Asif Vudi <asifaslam.aa81@gmail.com>
2024-01-04 20:01:11 +0600

    Add console logs to API functions

--- Ecom/api/cart/cart.js
+++ Ecom/api/cart/cart.js
@@ -2,33 +2,31 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { authURL } from "../instances/authURL";
 
 export const addToCart = async (productId, quantity) => {
+  console.log("=======addToCart API=======");
   try {
     const token = await AsyncStorage.getItem("user-token");
     const res = await authURL(token).post("/add-to-cart", {
       productId,
       quantity,
     });
-    console.log("=======addToCart response=======");
-    console.log(res);
+    console.log("...addToCart api response:", res);
     return res;
   } catch (error) {
-    console.log("=======addToCart error=======");
-    console.log(error);
+    console.log("...addToCart api error:", error);
     const errorResponse = error.response;
     return errorResponse;
   }
 };
 
 export const getCartDetails = async () => {
+  console.log("=======getCartDetails API=======");
   try {
     const token = await AsyncStorage.getItem("user-token");
     const res = await authURL(token).get("/get-cart-details");
-    console.log("=======getCartDetails response=======");
-    console.log(res);
+    console.log("...getCartDetails api response:", res);
     return res;
   } catch (error) {
-    console.log("=======getCartDetails error=======");
-    console.log(error);
+    console.log("...getCartDetails api error:", error);
     const errorResponse = error.response;
     return errorResponse;
   }
