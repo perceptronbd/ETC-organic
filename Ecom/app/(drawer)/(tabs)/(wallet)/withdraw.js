bf99d0a7bda1b4523bcfeafa15b101e0d90dfeb3
Asif Vudi <asifaslam.aa81@gmail.com>
2024-01-07 17:52:57 +0600

    Add withdrawAmount, bankId, and phoneNumber state values

--- Ecom/app/(drawer)/(tabs)/(wallet)/withdraw.js
+++ Ecom/app/(drawer)/(tabs)/(wallet)/withdraw.js
@@ -1,5 +1,6 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { router } from "expo-router";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Image, Pressable, View } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import { Divider, RadioButton } from "react-native-paper";
@@ -8,6 +9,7 @@ import {
   BankInfoCard,
   ContentModal,
   GoBack,
+  Loading,
   StyledButton,
   StyledInput,
   StyledText,
@@ -18,7 +20,13 @@ import { useCustomToast } from "../../../../hooks";
 import { useModal } from "../../../../hooks/useModal";
 
 const withdraw = () => {
+  const [balance, setBalance] = useState(0); //state value for balance fetched from the server for the logged in user
+  const [withdrawAmount, setWithdrawAmount] = useState(""); //state value for withdraw amount
   const [paymentType, setPaymentType] = useState(""); //state value for payment type selection for withdraw
+  const [bankId, setBankId] = useState(""); //state value for group selection of the bank accounts
+  const [phoneNumber, setPhoneNumber] = useState(""); //state value for phone number for bkash and nagad withdraw
+
+  const [loading, setLoading] = useState(false); //state value for loading
 
   const showToast = useCustomToast();
 
@@ -38,7 +46,29 @@ const withdraw = () => {
     hideModal: hideConfirmModal,
   } = useModal();
 
-  const openConfirmModal = () => {
+  useEffect(() => {
+    setLoading(true);
+    async function fetch() {
+      console.log("...index redeem fetching...");
+      AsyncStorage.getItem("user-data").then((data) => {
+        const userData = JSON.parse(data);
+        console.log("...index redeem userData", userData);
+        setBalance(userData?.taka);
+        setLoading(false);
+      });
+    }
+    fetch();
+  }, []);
+
+  const openConfirmModal = async () => {
+    if (!withdrawAmount && paymentType === "bank") {
+      showToast({
+        description: "দয়া করে Withdraw Amount প্রদান করুন​।",
+        variant: "warning",
+      });
+      return;
+    }
+
     if (paymentType === "bkash") {
       showBkash();
     } else if (paymentType === "nagad") {
@@ -53,7 +83,47 @@ const withdraw = () => {
     }
   };
 
+  const handlePhoneNumber = (value) => {
+    console.log(value);
+    setPhoneNumber(value);
+  };
+
   const goHome = () => {
+    const data = {
+      withdrawAmount,
+      paymentType,
+      phoneNumber,
+      bankId,
+    };
+
+    console.log("...withdraw goHome data:", data);
+
+    if (!withdrawAmount || !paymentType) {
+      showToast({
+        description: "দয়া করে Withdraw Amount প্রদান করুন​।",
+        variant: "warning",
+      });
+      return;
+    }
+    if (withdrawAmount <= 0) {
+      showToast({
+        description: "Withdraw Amount অবশ্যই 0 এর চেয়ে বড় হতে হবে​।",
+        variant: "warning",
+      });
+      return;
+    }
+    if (
+      (paymentType === "bkash" && !phoneNumber) ||
+      (paymentType === "nagad" && !phoneNumber)
+    ) {
+      showToast({
+        description: "দয়া করে সব Phone Number প্রদান করুন​।",
+        variant: "warning",
+      });
+      return;
+    }
+
+    console.log("...withdraw openConfirmModal data:", data);
     router.replace("/(drawer)/(tabs)/home");
     hideConfirmModal();
     hideBkash();
@@ -64,11 +134,16 @@ const withdraw = () => {
     <View style={tailwind`flex-1 justify-between px-3 pt-4`}>
       <View>
         <GoBack route={"/(drawer)/(tabs)/(wallet)"}>উত্তোলন</GoBack>
-        <BalanceWithdraw />
+        <BalanceWithdraw
+          setWithdrawAmount={setWithdrawAmount}
+          balance={balance}
+        />
         <Divider />
         <PaymentWithdraw
           paymentType={paymentType}
           setPaymentType={setPaymentType}
+          bankId={bankId}
+          setBankId={setBankId}
         />
       </View>
       <ContentModal visible={isBkash} hideModal={hideBkash}>
@@ -77,7 +152,13 @@ const withdraw = () => {
             bKash
           </StyledText>
           <StyledText>আপনার পার্সোনাল বিকাশ নাম্বার ইনপুট করুন</StyledText>
-          <StyledInput mode={"outlined"} label={"Phone No."} />
+          <StyledInput
+            mode={"outlined"}
+            label={"Phone No."}
+            onChangeText={(text) => {
+              handlePhoneNumber(text);
+            }}
+          />
           <StyledButton width={"md"} onPress={goHome}>
             কনফার্ম
           </StyledButton>
@@ -89,7 +170,11 @@ const withdraw = () => {
             Nagad
           </StyledText>
           <StyledText>আপনার পার্সোনাল নগদ নাম্বার ইনপুট করুন</StyledText>
-          <StyledInput mode={"outlined"} label={"Phone No."} />
+          <StyledInput
+            mode={"outlined"}
+            label={"Phone No."}
+            onChangeText={(text) => handlePhoneNumber(text)}
+          />
           <StyledButton width={"md"} onPress={goHome}>
             কনফার্ম
           </StyledButton>
@@ -113,20 +198,20 @@ const withdraw = () => {
           </StyledButton>
         </View>
       </ContentModal>
-
       <StyledButton onPress={openConfirmModal}>Withdraw Request</StyledButton>
+      <Loading isLoading={loading} />
     </View>
   );
 };
 
-const BalanceWithdraw = () => {
+const BalanceWithdraw = ({ setWithdrawAmount, balance }) => {
   return (
     <View style={tailwind`py-4`}>
       <View style={tailwind`flex-row items-end justify-between py-1`}>
         <StyledText variant="titleMedium">Available Balance :</StyledText>
         <View style={tailwind`flex-row items-end gap-2`}>
           <StyledText variant="titleLarge" type="b">
-            {1000}
+            {balance}
           </StyledText>
           <StyledText type="b">BDT</StyledText>
         </View>
@@ -140,6 +225,7 @@ const BalanceWithdraw = () => {
               height: 40,
             }}
             type="number"
+            onChangeText={(text) => setWithdrawAmount(text)}
           />
           <StyledText type="b">BDT</StyledText>
         </View>
@@ -148,26 +234,30 @@ const BalanceWithdraw = () => {
   );
 };
 
-const PaymentWithdraw = ({ paymentType, setPaymentType }) => {
+const PaymentWithdraw = ({
+  paymentType,
+  setPaymentType,
+  bankId,
+  setBankId,
+}) => {
   const [bankInfo, setBankInfo] = React.useState(bankData); //state value for bank info fetched from the server for the logged in user
   const [selectedBank, setSelectedBank] = React.useState(null); //state value for selected bank to withdraw
-  const [bank, setBank] = React.useState(""); //state value for group selection of the bank accounts
 
   const onBkash = () => {
     setPaymentType("bkash"); //set the payment type to bkash
-    setBank(""); //set the value of bank to empty string for group selection
+    setBankId(""); //set the value of bank to empty string for group selection
   };
 
   const onNagad = () => {
     setPaymentType("nagad"); //set the payment type to nagad
-    setBank(""); //set the value of bank to empty string for group selection
+    setBankId(""); //set the value of bank to empty string for group selection
   };
 
   const onSelectBank = (id) => {
     //find the bank info from the bankInfo array with the id
     const selectBank = bankInfo.find((item) => item.id === id);
 
-    setBank(id); //set the value of bank to the id of the selected bank for group selection
+    setBankId(id); //set the value of bank to the id of the selected bank for group selection
 
     setSelectedBank((prev) => (prev === selectBank ? null : selectBank)); //set the selected bank to the selectedBank state
     setPaymentType("bank"); //set the payment type to bank
@@ -217,7 +307,7 @@ const PaymentWithdraw = ({ paymentType, setPaymentType }) => {
         <StyledText variant="bodySmall" type="b" color={COLOR.neutralDark}>
           Bank
         </StyledText>
-        <RadioButton.Group onValueChange={onSelectBank} value={bank}>
+        <RadioButton.Group onValueChange={onSelectBank} value={bankId}>
           <ScrollView
             horizontal
             contentContainerStyle={{
